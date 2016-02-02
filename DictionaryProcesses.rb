1da4b862461b0b4d36ae1266ac098f89141ae0cd
Hasan <hefese@hotmail.com.tr>
2016-02-02 12:06:51 +0200

    emekleme....

---
+++ DictionaryProcesses.rb
@@ -0,0 +1,5 @@
+class DictionaryProcesses
+
+	# Temel CharSets
+	@@alphabetSet = ['a','b','c','ç','d','e','f','g','ğ','h','ı','i','j','k','l','m','n','o', 'ö', 'p','q','r','s', 'ş', 't','u','ü','v','w','x','y','z']
+	
